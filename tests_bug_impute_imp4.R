ae389365929cbbbff57d239f2f6e0759cf4ecd9d
FREMY Enora <EF249002@intra.cea.fr>
2019-12-03 14:58:00 +0100

    df in list

--- tests_bug_impute_imp4.R
+++ tests_bug_impute_imp4.R
@@ -2,20 +2,17 @@ library(DAPAR)
 library(DAPARdata)
 library(imp4p)
 
-
 #### data loading ####
+
+
 data("Exp1_R25_prot")
+data("Exp1_R25_pept")
 dat <- mvFilter(Exp1_R25_prot, type="allCond", th = 1)
+#dat <- mvFilter(Exp1_R25_pept, type="allCond", th = 1)
 qData <- Biobase::exprs(dat)
 
-#dim(qData) # 2384 6
-#head(qData)
-
-#### data pre ####
-qData <- cbind(qData, qData[,c(1,3,5)]) # add a third condition
-#head(qData)
+qData <- cbind(qData, qData[,c(1,3,5)]) # Third condition
 colnames(qData) <- c("A1","A2","A3","B1","B2","B3","C1","C2","C3")
-#head(qData)
 
 qData.NA <- is.na(qData) # keep NA for imputation
 
@@ -25,20 +22,22 @@ qData[,7:9] <- apply(qData[,7:9], 2, function(x) rnorm(x, mean = 1000, sd = 1))
 
 qData[qData.NA] <- NA
 rm(qData.NA)
-# different rnorm per condition
 
-#summary(qData)
-#boxplot(qData)
-# three different rnorm per condition OK
+df.init <- list(qData) # save NA tab in df.init
+
+
 
 #### imputation ####
+
+
+res.df <- list(slsa="",mi="")
 
 # wrapper.impute.slsa
 conds <- as.factor(c("A","A","A","B","B","B","C","C","C"))
 res.slsa <- impute.slsa(qData, conditions=conds, nknn=15, selec="all", weight=1, ind.comp=1)
 
-boxplot(qData)
-boxplot(res.slsa)
+res.df[[1]] <- res.slsa
+colnames(res.df$slsa) <- paste("slsa.protein",colnames(res.df$slsa), sep = "_")
 
 # wrapper.dapar.impute.mi
 conditions <- as.factor(c("A","A","A","B","B","B","C","C","C"))
@@ -53,11 +52,19 @@ res = estim.mix(tab = tab, tab.imp = dat.slsa, conditions = conditions,
                 x.step.pi = 300, nb.rei = 100)
 # Error in optim(init, fr, gr = NULL, x = (abs[, j] - xmin), pi_est = pi_init,  : 
 # L-BFGS-B nécessite des valeurs finies de 'fn'
+born = estim.bound(tab = tab, conditions = conditions, q = 0.95)
+proba = prob.mcar.tab(born$tab.upper, res)
+data.mi = mi.mix(tab = tab, tab.imp = dat.slsa, prob.MCAR = proba, 
+                 conditions = conditions, repbio = repbio, reptech = reptech, 
+                 nb.iter = nb.iter, nknn = nknn, weight = weight, selec = selec, 
+                 siz = siz, ind.comp = ind.comp, methodi = methodi, q = q, 
+                 progress.bar = progress.bar)
+data.final = impute.pa2(tab = data.mi, conditions = conditions, 
+                        q.min = q.min, q.norm = q.norm, eps = eps, distribution = distribution)
 
+res.df[[2]] <- data.final
+colnames(res.df$mi) <- paste("impute.mi.protein",colnames(res.df$mi), sep = "_")
 
-res.mi <- impute.mi(qData, conditions=conds, nknn=15, selec="all", weight=1, ind.comp=1)
+# res.mi <- impute.mi(qData, conditions=conds, nknn=15, selec="all", weight=1, ind.comp=1)
 # Error in optim(init, fr, gr = NULL, x = (abs[, j] - xmin), pi_est = pi_init,  : 
 # L-BFGS-B nécessite des valeurs finies de 'fn'
-
-boxplot(qData)
-boxplot(res.mi)
