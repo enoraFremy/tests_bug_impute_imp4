0be756f98fd4a3ade8c38b94d3d169075f9c75c4
FREMY Enora <EF249002@intra.cea.fr>
2019-12-04 11:57:16 +0100

    correspondance ordre Sample.name qData et pData et return(list(qData,pData))

--- mixed_df_generation.R
+++ mixed_df_generation.R
@@ -3,43 +3,52 @@ library(DAPARdata)
 library(imp4p)
 library(testthat)
 
-df_generation <- function(pData, qData, nCond, nRep, mismatch.nRep) {
+df_generation <- function(qData, pData, nCond, nRep, mismatch.nRep = FALSE) { 
   
-  # pData for ncol and colnames
-  pData.plus <- data.frame(matrix(nrow = nRep*nCond, ncol = ncol(pData)))
-  colnames(pData.plus) <- names(pData)
-  pData <- pData.plus ; rm(pData.plus)
-  pData$Bio.Rep <- c( 1:nrow(pData) )
-  
-  # replace Sample.Name by A1,...,A(nRep),B1,...,B(nRep), C1,... 
-  # and Condition by A,B,C,D...
-  Sample.name <- vector()
-  for (i in 1:nCond) {
-    #i=4
-    Sample.name <- c(Sample.name, paste0(LETTERS[i],1:nRep))
+  # Order of Sample.name of pData and qdata must be the same
+  if ( table(colnames(qData) == pData$Sample.name) ) {
+    
+    
+    # pData for ncol and colnames
+    pData.plus <- data.frame(matrix(nrow = nRep*nCond, ncol = ncol(pData)))
+    colnames(pData.plus) <- colnames(pData)
+    pData <- pData.plus ; rm(pData.plus)
+    pData$Bio.Rep <- c( 1:nrow(pData) )
+    
+    # set Sample.Name to A1,...,A(nRep),B1,...,B(nRep), C1,... 
+    # and Condition to A,B,C,D...
+    Sample.name <- vector()
+    for (i in 1:nCond) {
+      #i=4
+      Sample.name <- c(Sample.name, paste0(LETTERS[i],1:nRep))
+    }
+    pData$Sample.name <- Sample.name
+    pData$Condition <- substr(pData$Sample.name,1,1)
+    rownames(pData) <- pData$Sample.name
+    
+    # qData
+    qData.plus <- data.frame(matrix(nrow = nrow(qData), ncol = nRep*nCond))
+    rownames(qData.plus) <- rownames(qData) ## ! rownames(qData) Begin by 0 ! ##
+    qData.plus[,1:ncol(qData)] <- qData
+    
+    
+    
+    
+    
+    
+    colnames(qData.plus) <- pData$Sample.name
   }
-  pData$Sample.name <- Sample.name
-  pData$Condition <- substr(pData$Sample.name,1,1)
-  rownames(pData) <- pData$Sample.name
-
-  # qData
-  
+  res <- list(pData=pData,qData=qData.plus)
+  return(res)
   
 }
 
+#------------------------------------------------------------
 data("Exp1_R25_pept")
-nCond = 3
-nRep = 3
+#nCond = 3
+#nRep = 3
 qData <- Biobase::exprs(Exp1_R25_pept)
 pData <- Biobase::pData(Exp1_R25_pept)
 
 #------------------------------------------------------------
-df_generation(qData, pData, 3, 3, FALSE)
-
-
-qData <- cbind(qData, qData[,c(1,3,5)]) # Third condition
-colnames(qData) <- c("A1","A2","A3","B1","B2","B3","C1","C2","C3")
-pData <- rbind(pData, pData[1:nbCond,])
-pData$Condition <- c(rep("A",nbCond), rep("B", nbCond), rep("C", nbCond))
-pData$Sample.name <- paste0(pData$Condition, 1:nbCond)
-rownames(pData) <- pData$Sample.name
+res <- df_generation(qData, pData, 3, 3)
